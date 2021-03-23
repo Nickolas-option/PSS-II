50e8a0ffc10fd1f1f6a4d55a5251e6ad6bc3ff5f
Nickolas-option <69462766+Nickolas-option@users.noreply.github.com>
2021-03-23 20:40:30 +0300

    Create Room.h

---
+++ HW3/Room.h
@@ -0,0 +1,47 @@
+//
+// Created by nikae on 08.03.2021.
+//
+
+#include "User.h"
+
+#ifndef HW2_ROOM_H
+#define HW2_ROOM_H
+
+#endif //HW2_ROOM_H
+
+class Room {
+public:
+    Room() = default;
+
+    ~Room() = default;
+
+    Room(int number);
+
+    string getRoomNumber();
+
+    string tryToEnter(User person);
+
+protected:
+    AccessLevel accessNeeded;
+    int roomNumber;
+};
+
+class LectureRoom : public Room {
+public:
+    LectureRoom(int number, AccessLevel accessNeeded = noLevel);
+};
+
+class ConferenceRoom : public Room {
+public:
+    ConferenceRoom(int number, AccessLevel accessNeeded = yellowLevel);
+};
+
+class Cabinet : public Room {
+public:
+    Cabinet(int number, AccessLevel accessNeeded = noLevel);
+};
+
+class DirectorCabinet : public Room {
+public:
+    DirectorCabinet(int number, AccessLevel accessNeeded = redLevel);
+}; 
